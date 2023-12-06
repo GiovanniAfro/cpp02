225e0aaa8218c276a21446959288466e4450a069
Giovanni Cavanna <giovanni.cacciacavanna@gmail.com>
2023-12-06 17:08:58 +0100

    risolto calcoli, maledetti float

--- ex02/Fixed.cpp
+++ ex02/Fixed.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Fixed.cpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gcavanna <gcavanna@student.42firenze.it    +#+  +:+       +#+        */
+/*   By: gcavanna <gcavanna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/04 14:49:25 by gcavanna          #+#    #+#             */
-/*   Updated: 2023/12/06 11:07:36 by gcavanna         ###   ########.fr       */
+/*   Updated: 2023/12/06 17:05:21 by gcavanna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,33 +14,27 @@
 
 Fixed::Fixed() : _value(0)
 {
-    std::cout << "Default constructor called" << std::endl;
 }
 
 Fixed::Fixed(const int value) : _value(value << fractionalBits)
 {
-    std::cout << "Int conctructor called" << std::endl;
 }
 
-Fixed::Fixed(const float value) : _value(value * (1 << fractionalBits))
+Fixed::Fixed(const float value) : _value(roundf(value * (1 << fractionalBits)))
 {
-    std::cout << "Float constructor called" << std::endl;
 }
 
 Fixed::Fixed(const Fixed& src)
 {
-    std::cout << "Copy constructor called" << std::endl;
     *this = src; //è un'espressione che indica l'assegnazione di un oggetto al proprio oggetto corrente nella programmazione. Nel contesto del copy constructor che hai definito, questa espressione è utilizzata per copiare il contenuto di un oggetto Fixed (rappresentato da src) nell'oggetto corrente, cioè *this. this: È un puntatore implicito all'oggetto corrente. In questo contesto, this rappresenta l'indirizzo dell'oggetto corrente di tipo Fixed. *this: L'operatore di dereferenziazione * applicato a this restituisce l'oggetto effettivo, non l'indirizzo. Quindi, *this rappresenta l'oggetto corrente di tipo Fixed
 }
 
 Fixed::~Fixed() 
 {
-    std::cout << "Destructor called" << std::endl;
 }
 
 Fixed& Fixed::operator=(const Fixed& rhs) 
 {
-    std::cout << "Copy assignment operator called" << std::endl;
     if (this != &rhs)
         this->_value = rhs.getRawBits();
     return *this;
@@ -48,19 +42,17 @@ Fixed& Fixed::operator=(const Fixed& rhs)
 
 int Fixed::getRawBits() const 
 {
-    std::cout << "getRawBits member function called" << std::endl;
     return this->_value;
 }
 
 void Fixed::setRawBits(int const raw)
 {
-    std::cout << "setRawBits member function called" << std::endl;
     this->_value = raw;
 }
 
 float Fixed::toFloat() const
 {
-    return static_cast<float>(this->_value) / (1 << fractionalBits);
+    return (static_cast<float>(this->_value) / static_cast<float>(1 << fractionalBits));
 }
 
 int Fixed::toInt() const
@@ -104,24 +96,24 @@ bool Fixed::operator!=(const Fixed& other) const
 }
 
 // Implementazione degli operatori aritmetici
-Fixed Fixed::operator+(const Fixed& other) const
+Fixed	Fixed::operator+(const Fixed& rhs) const
 {
-    return Fixed(_value + other._value);
+	return Fixed(this->toFloat() + rhs.toFloat());
 }
 
-Fixed Fixed::operator-(const Fixed& other) const
+Fixed	Fixed::operator-(const Fixed& rhs) const
 {
-    return Fixed(_value - other._value);
+	return Fixed(this->toFloat() - rhs.toFloat());
 }
 
-Fixed Fixed::operator*(const Fixed& other) const
+Fixed	Fixed::operator*(const Fixed& rhs) const
 {
-    return Fixed((_value * other._value) >> fractionalBits);
+	return Fixed(this->toFloat() * rhs.toFloat());
 }
 
-Fixed Fixed::operator/(const Fixed& other) const
+Fixed	Fixed::operator/(const Fixed& rhs) const
 {
-    return Fixed((_value << fractionalBits) / other._value);
+	return Fixed(this->toFloat() / rhs.toFloat());
 }
 
 // Implementazione degli operatori di incremento/decremento
