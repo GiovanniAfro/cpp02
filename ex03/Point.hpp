caca101f1ad130d7573d63d10775a13a92835abd
giovanniAfro <giovanni.cacciacavanna@gmail.com>
2023-12-08 15:34:11 +0100

    Point.hpp

--- ex03/Point.hpp
+++ ex03/Point.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Point.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gcavanna <gcavanna@student.42.fr>          +#+  +:+       +#+        */
+/*   By: gcavanna <gcavanna@student.42firenze.it    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/06 17:41:47 by gcavanna          #+#    #+#             */
-/*   Updated: 2023/12/06 17:44:11 by gcavanna         ###   ########.fr       */
+/*   Updated: 2023/12/08 15:33:48 by gcavanna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,21 @@
 class Point
 {
 	private:
-		
-}
+		const Fixed	_x;
+		const Fixed _y;
 
+	public:
+		Point(void);
+		Point(const float x, const float y);
+		Point(const Point& src);
+		~Point(void);
 
+		Point& operator=(const Point& rhs);
 
+		float	getX(void) const;
+		float	getY(void) const;
+};
 
-
-
-
+bool bsp( Point const a, Point const b, Point const c, Point const point);
 
 #endif
